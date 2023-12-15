c38b167d8848eb3e5f1e64ad9b168c526f415856
n83a <134180430+n83a@users.noreply.github.com>
2023-12-15 18:52:27 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function li1(type){
-if(type == 'x'){
-window.open("https://twitter.com/naaaaaao831934","_blank");
-}
-if(type == 't'){
-window.open("https://www.threads.net/@naaa83a","_blank");
-}
-}
