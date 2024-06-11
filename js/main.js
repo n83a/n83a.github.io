8ba0b4ebe6c38b4a22265552fa1bf7b2445e4a27
Naaaaaao <134180430+n83a@users.noreply.github.com>
2024-06-11 16:07:47 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const hour = new Date().getHours();
 
+if(hour >= 3　&& hour < 11){
+document.getElementById('t1').textContent ="おはようございます";
+  
+}else if(hour >=11 && hour <17){
+document.getElementById('t1').textContent ="こんにちは";
+  
+}else{
+document.getElementById('t1').textContent ="こんばんは";
+  
+}
